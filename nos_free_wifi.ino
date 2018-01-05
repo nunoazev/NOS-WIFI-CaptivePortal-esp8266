71470712a912b74bd8df8e62cac7a368eeb9ba92
nunoazev <nuno@pcode.pt>
2018-01-05 09:31:57 +0000

    ok

--- nos_free_wifi.ino
+++ nos_free_wifi.ino
@@ -7,8 +7,8 @@ const char* ssid     = "NOS_WIFI_Fon";
 const char* password = "";
 
 //nos login captive
-const String NOSUSR  = "username";
-const String NOSPASS  = "password";
+const String NOSUSR  = "";
+const String NOSPASS  = "";
 //gerada aqui -> https://www.grc.com/fingerprints.htm
 const String fingerprint  = "E6:E9:08:D6:6A:7E:E7:5D:34:5D:F0:DB:28:14:DA:2B:8D:87:3A:02";
 
@@ -63,18 +63,31 @@ void setup() {
   http.addHeader("Cookie", http.header(SET_COOKIE));
   httpCode = http.POST("userFake=" + NOSUSR + "&UserName=NOS/" + NOSUSR + "&Password=" + NOSPASS); //Send the request
   Serial.println(httpCode);   //Print 302 movido temp -2*/
-
   delay(2000);
 
 
-  http.begin(http.header(LOCATION));
+  uri = http.header(LOCATION);
   http.collectHeaders(HEADER_NAMES, 2);
   httpCode = http.GET();
+  http.begin(uri);
+
+  Serial.print("1-redirect=");
+  Serial.println(uri);
+  Serial.println(httpCode);   //Print 302 movido temp -2*/
+  delay(2000);
+
 
   uri = http.header(LOCATION);
-  Serial.print("redirect=");
+  http.collectHeaders(HEADER_NAMES, 2);
+  httpCode = http.GET();
+  http.begin(uri);
+
+  Serial.print("2-redirect=");
   Serial.println(uri);
-  Serial.println(httpCode);   //Print 200 sucesso -1
+  Serial.println(httpCode);   //Print 302 movido temp -2*/
+  delay(2000);
+
+  
   http.end();  //Close connection
 
 
